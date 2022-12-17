81787a79ebea55aa160f6306a81781a7e9ea2a0d
Luana <lugamoreira@gmail.com>
2022-12-17 12:02:52 -0500

    commit tree 0f57c90ff76899d9726bd96db8950eba930405e4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+let name = "Luana"
+newHeader.id = 'victory'
+newHeader.textContent = `${name} is the champion`;
